db57ef8cfad9bee6aa77a9e94e066874cb9d3b8f
Connor Twele <connortwele@gmail.com>
2020-08-23 13:47:40 -0400

    Updates

---
+++ precache-manifest.1c3f5323ab8c3ed6b423d8a398fc85e2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8c46d7ccecc2ceb1ee4fe76609bdac",
+    "url": "/react-drum-kit/index.html"
+  },
+  {
+    "revision": "136c0a1d6fb92f718a29",
+    "url": "/react-drum-kit/static/css/main.57f9f768.chunk.css"
+  },
+  {
+    "revision": "6fc970d2635ad51aa01e",
+    "url": "/react-drum-kit/static/js/2.c3be3493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-drum-kit/static/js/2.c3be3493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136c0a1d6fb92f718a29",
+    "url": "/react-drum-kit/static/js/main.a5f740e8.chunk.js"
+  },
+  {
+    "revision": "4f4f6c0ad2c6b0ee226d",
+    "url": "/react-drum-kit/static/js/runtime-main.74add1fd.js"
+  },
+  {
+    "revision": "52f8783da34197ad1c9d726aa4c484ca",
+    "url": "/react-drum-kit/static/media/sound1.52f8783d.mp3"
+  },
+  {
+    "revision": "b8d7845c7a213c7264b804335e0b7d66",
+    "url": "/react-drum-kit/static/media/sound2.b8d7845c.mp3"
+  },
+  {
+    "revision": "ddbed05251cbb3e9b568cb77221be02d",
+    "url": "/react-drum-kit/static/media/sound3.ddbed052.mp3"
+  },
+  {
+    "revision": "6be0017e74ccf99317dcc98b9cc104d1",
+    "url": "/react-drum-kit/static/media/sound4.6be0017e.mp3"
+  },
+  {
+    "revision": "11c7389f9ed245c4136d0b3ae1b398c2",
+    "url": "/react-drum-kit/static/media/sound5.11c7389f.mp3"
+  },
+  {
+    "revision": "82d21bfa1e4c1b6310181455a58b652d",
+    "url": "/react-drum-kit/static/media/sound6.82d21bfa.mp3"
+  },
+  {
+    "revision": "375ae37d60a3ebe514d057c171128840",
+    "url": "/react-drum-kit/static/media/sound7.375ae37d.mp3"
+  },
+  {
+    "revision": "3ca260e20549d71bb4db2de493dbce7a",
+    "url": "/react-drum-kit/static/media/sound8.3ca260e2.mp3"
+  },
+  {
+    "revision": "f3af42961fb9592e8081cba12e490982",
+    "url": "/react-drum-kit/static/media/sound9.f3af4296.mp3"
+  }
+]);
